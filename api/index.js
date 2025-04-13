91ecb16e686763ae2c77a2275ae9c06fa4ced65e
Proxyers <ryanyuqiao819@gmail.com>
2025-04-14 00:29:58 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,50 +1,33 @@
 import { createServer } from "node:http";
+import { join } from "node:path";
 import { hostname } from "node:os";
 import wisp from "wisp-server-node";
 import Fastify from "fastify";
 import fastifyStatic from "@fastify/static";
 
+// 引入静态资源路径
 import { publicPath } from "ultraviolet-static";
 import { uvPath } from "@titaniumnetwork-dev/ultraviolet";
 import { epoxyPath } from "@mercuryworkshop/epoxy-transport";
 import { baremuxPath } from "@mercuryworkshop/bare-mux/node";
 
-const fastify = Fastify({
-serverFactory: (handler) => {
-return createServer()
-.on("request", (req, res) => {
-res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
-res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
-handler(req, res);
-})
-.on("upgrade", (req, socket, head) => {
-if (req.url.endsWith("/wisp/")) wisp.routeRequest(req, socket, head);
-else socket.end();
-});
-},
-});
+const fastify = Fastify();
 
+// 注册静态资源
 fastify.register(fastifyStatic, {
 root: publicPath,
 decorateReply: true,
 });
-
-fastify.get("/uv/uv.config.js", (req, res) => {
-return res.sendFile("uv/uv.config.js", publicPath);
-});
-
 fastify.register(fastifyStatic, {
 root: uvPath,
 prefix: "/uv/",
 decorateReply: false,
 });
-
 fastify.register(fastifyStatic, {
 root: epoxyPath,
 prefix: "/epoxy/",
 decorateReply: false,
 });
-
 fastify.register(fastifyStatic, {
 root: baremuxPath,
 prefix: "/baremux/",
@@ -52,6 +35,6 @@ decorateReply: false,
 });
 
 export default async function handler(req, res) {
-await fastify.ready();
+await fastify.ready(); // 确保插件已注册
 fastify.server.emit("request", req, res);
 }
