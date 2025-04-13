2dcd234dd646c8ccddacdf9c54a77fac17264ba4
Proxyers <ryanyuqiao819@gmail.com>
2025-04-14 04:20:41 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import { uv } from '@titaniumnetwork-dev/ultraviolet';
 import { join } from 'path';
 import { fileURLToPath } from 'url';
 import { createTransport } from '@mercuryworkshop/epoxy-transport';
+import fastifyStatic from '@fastify/static';
 
 const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
@@ -13,6 +14,11 @@ prefix: '/',
 bare: server,
 publicPath: join(__dirname, '../public'),
 transport: createTransport()
+});
+
+fastify.register(fastifyStatic, {
+root: join(__dirname, '../public'),
+prefix: '/public/',
 });
 
 export default function handler(req, res) {
