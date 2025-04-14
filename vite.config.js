311c0772c128dc120349964d2e129635b30c5385
Proxyers <ryanyuqiao819@gmail.com>
2025-04-14 20:24:14 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: './',
+  base: './',  // 确保基础路径是相对路径
   build: {
-    outDir: 'dist',
+    outDir: 'dist',  // 输出目录为 dist
   },
 })
